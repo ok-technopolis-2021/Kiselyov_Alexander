df5941d8d6cafd8c4bd48ff1c8870d13e940ce2e
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2021-11-04 19:42:47 +0300

    Внесены исправления

--- dist/toggle.js
+++
@@ -1,9 +0,0 @@
-var checkbox = document.getElementById('toggle');
-
-checkbox.onclick = function () {
-    if (this.checked) {
-        document.documentElement.setAttribute('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('theme', 'light');
-    }
-}
