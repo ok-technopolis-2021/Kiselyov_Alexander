f2a1e78e204d0ce0fd282d4826dbcf764985e0e6
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2021-11-04 19:12:16 +0300

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,11 @@
-var checkbox = document.getElementById('toggle');
+document.addEventListener("DOMContentLoaded", function(event) {
+    var checkbox = document.getElementById('toggle');
 
-checkbox.onclick = function () {
-    if (this.checked) {
-        document.documentElement.setAttribute('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('theme', 'light');
+    checkbox.onclick = function () {
+        if (this.checked) {
+            document.documentElement.setAttribute('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('theme', 'light');
+        }
     }
-}
+});
